9532a9fa82fd5cbe9cddff8e9ef1778bdd74081f
ktakashi <ktakashi@ymail.com>
2013-05-27 19:55:49 +0200

    added split panel example

---
+++ example/split-panel.scm
@@ -0,0 +1,15 @@
+(import (clos user) (turquoise)
+	(sagittarius object))
+
+(let* ((window (make <frame> :name "Panel example"))
+       (panel1 (make <split-panel> :name "Panel1"
+		     :panel1 (make <panel> :background 'black)
+		     :panel1 (make <panel> :background (make <rgb> :g 255))))
+       (panel2 (make <split-panel> :name "Panel1"
+		     :virtical #t
+		     ;; to panel must have size
+		     :adjust-size #t
+		     :panel1 panel1
+		     :panel2 (make <panel> :background (make <rgb> :b 255)))))
+  (add! window panel2)
+  (window))
